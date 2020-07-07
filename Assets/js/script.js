3ca09f080922f34104a961e093abd74e56e48426
zhim57 <65269162+zhim57@users.noreply.github.com>
2020-07-07 16:13:54 -0400

    added the color code

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -9,22 +9,19 @@ $(document).ready(function () {
     var savedObject = JSON.parse(localStorage.getItem("savedObject"));
     console.log(savedObject);
     var hoursS;
+    var checkTime = moment().format('LT');
+    console.log(checkTime);
+    var checkTime1 = moment().format("H");
+    console.log(checkTime1);
 
     var hoursJ = ["0900", "1000", "1100", "1200", "1300", "1400", "1500", "1600", "1700"]
 
-    if (savedObject !== null) {
-        hourTextArr = savedObject;
-    }
-    else {
-        hourTextArr = new Array(9);
-    }
-    console.log(hourTextArr);
+
     for (var hour = 9; hour <= 17; hour++) {
         i = (hour - 9);
         var displayHoursJ = $("<div class= hour-label>");
         displayHoursJ.addClass("data-hour", hoursJ[i]);
         displayHoursJ.addClass("nomar", hoursJ[i]);
-
 
         var dispRowJ = $("<div class=row>");
         dispRowJ.attr("id", hoursJ[i]);
@@ -34,17 +31,26 @@ $(document).ready(function () {
 
         textArea.attr("data-name", hoursJ[i])
 
-
-
-
         displayCol1.text(hoursJ[i]);
         var saveBtnJ = $("<button class=col-sm-2>");
         saveBtnJ.addClass("nomar");
-        ;
+
         saveBtnJ.addClass("saveBtn far fa-save saveIcon");
-        ;
-        var note = $(this).attr("data-name");
-        console.log(note);
+
+        if (hour<checkTime1){
+            $(textArea).css("background-color" , " lightGray");
+        }
+        else if(hour == checkTime1){
+            $(textArea).css("background-color" , "lightSalmon");
+
+        }
+        else{
+            $(textArea).css("background", "lightGreen");
+        }
+
+
+
+
 
 
         $(".container").append(dispRowJ);

7f3024a03f64cdb97ab04ea9af7264620a2d90f3
zhim57 <65269162+zhim57@users.noreply.github.com>
2020-07-07 06:17:42 -0400

    cleaned code, added remarks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,41 +1,41 @@
-
+// function to wait for the javascript till document has loaded fully
 $(document).ready(function () {
+
+    // displaying the "today" date in the header
 
     var todayDate = $("#currentDay");
     var momentJ = moment().format("dddd , MMMM Do YYYY");
     todayDate.text(momentJ);
-    var a = moment().format();
-    console.log(a);
-    var savedObject = JSON.parse(localStorage.getItem("savedObject"));
-    console.log(savedObject);
+
+
+    // setting variables
+
     var hoursS;
-    var checkTime = moment().format('LT');
-    console.log(checkTime);
     var checkTime1 = moment().format("H");
-    console.log(checkTime1);
 
     var hoursJ = ["0900", "1000", "1100", "1200", "1300", "1400", "1500", "1600", "1700"]
 
+    // creating the rows and adding classes and Ids
 
     for (var hour = 9; hour <= 17; hour++) {
         i = (hour - 9);
-        var displayHoursJ = $("<div class= hour-label>");
-        displayHoursJ.addClass("data-hour", hoursJ[i]);
-        displayHoursJ.addClass("nomar", hoursJ[i]);
 
         var dispRowJ = $("<div class=row>");
         dispRowJ.attr("id", hoursJ[i]);
+
         var displayCol1 = $("<p class=col-sm-2 >");
         displayCol1.addClass("hour-label");
-        var textArea = $("<textarea class=col-md-8>");
+        displayCol1.text(hoursJ[i]);
 
+        var textArea = $("<textarea class=col-md-8>");
         textArea.attr("data-name", hoursJ[i])
 
-        displayCol1.text(hoursJ[i]);
         var saveBtnJ = $("<button class=col-sm-2>");
         saveBtnJ.addClass("nomar");
 
         saveBtnJ.addClass("saveBtn far fa-save saveIcon");
+
+        // changing the background colors for past hours , present hour and future hours tasks
 
         if (hour < checkTime1) {
             $(textArea).css("background-color", " lightGray");
@@ -48,20 +48,15 @@ $(document).ready(function () {
             $(textArea).css("background", "lightGreen");
         }
 
-
-
-
-
+        // appending the new elements 
 
         $(".container").append(dispRowJ);
         $(dispRowJ).append(displayCol1);
         $(dispRowJ).append(textArea);
         $(dispRowJ).append(saveBtnJ);
-
-
     };
 
-    // $saveHandler
+    // funktion for saving the input fields contents to local storrage
 
     $(".saveBtn").click(function () {
 
@@ -73,14 +68,23 @@ $(document).ready(function () {
 
     });
 
+    // function for retrieving the saved data from local storrage and populating the fields
+
     var displayPlanner = function () {
 
         for (var i = 0; i < hoursJ.length; i++) {
             var hoursR = (localStorage.getItem(hoursJ[i]));
             $(`#${hoursJ[i]} textarea`).val(hoursR);
         }
+        
+        
+        
+        //clear All function for emptying all fields at once
+        
         $("#clearAll").click(function (event) {
             (event).preventDefault();
+            var doProceed = confirm("Are you sure you wish to proceed with clearing of All fields, all stored tasks  will be deleted? ");
+            if (doProceed){
             for (var i = 0; i < hoursJ.length; i++) {
                 localStorage.setItem(hoursJ[i], "");
 
@@ -88,6 +92,9 @@ $(document).ready(function () {
 
                 $(`#${hoursJ[i]} textarea`).val(hoursC);
             }
+        }else{
+            alert("OK, 'clear All ' aborted.")
+        }
         });
 
 
